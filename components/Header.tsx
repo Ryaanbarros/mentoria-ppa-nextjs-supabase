71e3a85a857fecf611020a6521524f7680778d7d
Ryan Barros <ryan.santos.barros@hotmail.com>
2024-03-12 16:03:35 -0300

    att

--- components/Header.tsx
+++ components/Header.tsx
@@ -6,7 +6,10 @@ export default function Header() {
     <div className="flex flex-col gap-16 items-center">
       
       <div className="text-3xl lg:text-4xl text-center">
-        Seja bem vindo ao sistema <br /><p>Ryan de Resgate</p>      
+        Seja bem vindo ao sistema <br /><p>de cadastro</p>     
+        <br />
+        <br />
+        <p>Criei este sistema para armazenar dados em um banco Postgres</p> 
       </div>
       </div>
   );
